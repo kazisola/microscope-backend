4e0afa621befdde84ddcd57849945c3256f6a4d9
Kazi Solayman <kazisolayman123@gmail.com>
2026-08-26 16:17:30 -0500

    Created auth service with user register service

--- src/microscope_backend/routers/user.py
+++ src/microscope_backend/routers/user.py
@@ -1,17 +1,21 @@
-from fastapi import APIRouter, status
+from typing import Annotated
+from fastapi import APIRouter, status, Depends
 from microscope_backend.schemas.user import UserCreate, UserPrivate, Token
-
+from microscope_backend.database import get_db
+from sqlalchemy.ext.asyncio import AsyncSession
+from microscope_backend.services import auth_service
 
 router = APIRouter(prefix="/api/v1/users", tags=["Users"])
 
-@router.get("/hello")
-async def hello():
-    return {"message": "Hello!"}
+@router.post("/register", response_model=UserPrivate, status_code=status.HTTP_201_CREATED)
+async def register(
+    register_data: UserCreate,
+    db: Annotated[AsyncSession, Depends(get_db)]
+    ):
+    new_user = await auth_service.register_user(register_data, db)
+    return new_user
 
-@router.post("", response_model=UserPrivate, status_code=status.HTTP_201_CREATED)
-async def register_user(register_data: UserCreate):
-    pass
 
 @router.post("/login", response_model=Token, status_code=status.HTTP_200_OK)
-async def login_user():
+async def login():
     pass

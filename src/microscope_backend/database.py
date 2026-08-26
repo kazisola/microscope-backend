982cf8c07d97c2784131f61c244d35a226930b09
Kazi Solayman <kazisolayman123@gmail.com>
2026-08-26 14:16:03 -0500

    Configured database connection

---
+++ src/microscope_backend/database.py
@@ -0,0 +1,25 @@
+from collections.abc import AsyncGenerator
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from microscope_backend.config import settings
+from sqlalchemy.orm import DeclarativeBase
+
+
+engine = create_async_engine(
+    settings.database_url,
+    echo=settings.debug,
+    pool_pre_ping=True
+)
+
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    autoflush=False,
+    expire_on_commit=False
+)
+
+class Base(DeclarativeBase):
+    pass
+
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with AsyncSessionLocal() as session:
+        yield session

0012da2bb619f55477f06d97def809748fbd3073
Kazi Solayman <kazisolayman123@gmail.com>
2026-08-27 16:32:08 -0500

    Created logged in user endpoint with service

--- src/microscope_backend/services/auth_service.py
+++ src/microscope_backend/services/auth_service.py
@@ -4,7 +4,7 @@ from microscope_backend.database import get_db
 from sqlalchemy import select, func, or_
 from sqlalchemy.ext.asyncio import AsyncSession
 from microscope_backend.models import User
-from microscope_backend.core.auth import hash_password, verify_password, create_access_token
+from microscope_backend.core.auth import hash_password, verify_password, create_access_token, CurrentUser
 from microscope_backend.schemas.user import UserCreate
 from fastapi.security import OAuth2PasswordRequestForm
 from datetime import timedelta
@@ -70,3 +70,21 @@ async def login_user(
         )
 
     return access_token
+
+
+async def get_user_service(
+    current_user: CurrentUser,
+    db: Annotated[AsyncSession, Depends(get_db)]
+    ):
+    result = await db.execute(
+        select(User)
+        .where(User.id == current_user.id)
+    )
+    user = result.scalar_one_or_none()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found"
+        )
+    
+    return user

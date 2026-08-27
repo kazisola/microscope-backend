a277ea13ae364260767345e80745372c4be576a9
Kazi Solayman <kazisolayman123@gmail.com>
2026-08-27 03:10:53 -0500

    Created verify password util

--- src/microscope_backend/core/auth.py
+++ src/microscope_backend/core/auth.py
@@ -8,6 +8,9 @@ password_hash = PasswordHash.recommended()
 def hash_password(password: str) -> str:
     return password_hash.hash(password)
 
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return password_hash.verify(plain_password, hashed_password)
+
 def create_access_token(data: dict, expires_delta: timedelta | None = None) -> str:
     to_encode = data.copy()
     if expires_delta:

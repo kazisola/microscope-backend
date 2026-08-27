638a61634ff3878760ed530a6e805304ba09b7c6
Kazi Solayman <kazisolayman123@gmail.com>
2026-08-27 16:15:56 -0500

    created get current user dependency

--- src/microscope_backend/core/auth.py
+++ src/microscope_backend/core/auth.py
@@ -1,8 +1,14 @@
+from typing import Annotated
+from fastapi import Depends, status, HTTPException
 from pwdlib import PasswordHash
 import jwt
 from datetime import datetime, timedelta, UTC
 from microscope_backend.config import settings
 from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from microscope_backend.database import get_db
+from microscope_backend.models import User
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/users/login")
 
@@ -44,3 +50,41 @@ def verify_access_token(token: str) -> str | None:
         return None
     else:
         return payload.get("sub")
+
+
+async def get_current_user(
+    token: Annotated[str, Depends(oauth2_scheme)],
+    db: Annotated[AsyncSession, Depends(get_db)]
+    ) -> User:
+    user_id = verify_access_token(token)
+
+    if user_id is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect or expired token",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+
+    try:
+        user_id_int = int(user_id)
+    except (ValueError, TypeError):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect or expired token",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+
+    result = await db.execute(
+        select(User)
+        .where(User.id == user_id_int)
+    )
+    user = result.scalar_one_or_none()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="No user found"
+        )
+
+    return user
+
+CurrentUser = Annotated[User, Depends(get_current_user)]
